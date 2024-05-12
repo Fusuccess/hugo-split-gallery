a37ad489431b6138e045773dc04ddb6eef2dd236
Fusuccess <fu2000520@tom.com>
2024-05-12 16:27:16 +0800

    feat(main): add Chinese translation

---
+++ assets/hugo-split-gallery/fancybox.zh.js
@@ -0,0 +1,14 @@
+$.fancybox.defaults.i18n['zh'] = {
+    CLOSE: "关闭",
+    NEXT: "下一个",
+    PREV: "上一个",
+    ERROR: "内容无法加载。请稍后再试。",
+    PLAY_START: "启动关灯片放映",
+    PLAY_STOP: "暂停幻灯片放映",
+    FULL_SCREEN: "全屏",
+    THUMBS: "缩略图",
+    DOWNLOAD: "下载",
+    SHARE: "分享",
+    ZOOM: "缩放"
+};
+$.fancybox.defaults.lang = "zh"
